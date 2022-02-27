283ef921a11c9b58fcc7eb3b5ab1a23dbcde64f7
coolpig123 <89049656+coolpig123@users.noreply.github.com>
2022-02-27 21:18:11 +0200

    ui and functionality added

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const paragraph = document.getElementsByTagName("p")
 const buttons = document.querySelectorAll('button');
+let answer 
 buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    paragraph[0].innerHTML += button.innerHTML
-  });
+  if(button.innerHTML == "="){
+    button.addEventListener('click', () => {
+      answer = eval(paragraph[0].innerHTML)
+      paragraph[0].innerHTML = answer
+    });
+  }else if(button.innerHTML == "AC"){
+    button.addEventListener('click', () => {
+      paragraph[0].innerHTML = ""
+    });
+  }else{
+    button.addEventListener('click', () => {
+      paragraph[0].innerHTML += button.innerHTML
+    });
+  }
 });
